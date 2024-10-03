16f9d331475088fad8b7efc6c85d0ab70a0fbfe4
Kaspatcho <nosilaw7777@gmail.com>
2024-10-03 17:03:32 -0300

    adding authentication middleware
    it ensures people who access certain routes are authenticated.

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnimeController;
 use App\Http\Controllers\LoginController;
+use App\Http\Middleware\EnsureIsAuthenticated;
 
-Route::get('/', [AnimeController::class, 'index'])->name('anime.index');
-Route::get('/anime/{id_anime}', [AnimeController::class, 'show'])->name('anime.show');
-Route::get('/anime/{id_anime}/episodio/{id_episodio}', [AnimeController::class, 'episode'])->name('anime.episode');
 Route::get('/', [LoginController::class, 'login'])->name('login');
 Route::post('/', [LoginController::class, 'authenticate'])->name('login.auth');
 Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+
+Route::middleware(EnsureIsAuthenticated::class)->group(function() {
+    Route::get('/lista', [AnimeController::class, 'index'])->name('anime.index');
+    Route::get('/anime/{id_anime}', [AnimeController::class, 'show'])->name('anime.show');
+    Route::get('/anime/{id_anime}/episodio/{id_episodio}', [AnimeController::class, 'episode'])->name('anime.episode');
+});
