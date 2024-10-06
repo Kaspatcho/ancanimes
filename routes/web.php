22af7fb161f6ecce0b5e070bedcba97bc36f09d3
Kaspatcho <nosilaw7777@gmail.com>
2024-10-06 00:34:13 -0300

    removing Auth middleware and adding laravel default one

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ Route::post('/', [LoginController::class, 'authenticate'])->name('login.auth');
 Route::post('/signup', [LoginController::class, 'signup'])->name('signup');
 Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 
-Route::middleware(EnsureIsAuthenticated::class)->group(function() {
+Route::middleware('auth')->group(function() {
     Route::get('/lista', [AnimeController::class, 'index'])->name('anime.index');
     Route::get('/anime/{anime}', [AnimeController::class, 'show'])->name('anime.show');
     Route::get('/anime/episodio/{episodio}', [AnimeController::class, 'episode'])->name('anime.episode');
